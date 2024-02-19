ba7b030eb64ac4d6b5030a8e351d113daf265c0c
Colan Worstell <cworstellprogramming@gmail.com>
2024-02-19 16:17:42 -0600

    permission error

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -14,10 +14,10 @@ if __name__ == '__main__':
 
     response = requests.get(sys.argv[1])
 
-    if response.status_code == 404:
+    if response.status_code == 200:
+        print(response.json()['location'])
+    elif response.status_code == 404:
         print("Not found")
     elif response.status_code == 403:
         time = (int(response.headers['X-RateLimit-Reset']) - int(time.time()))
         print("Reset in {} min".format(time//60))
-    elif response.status_code == 200:
-        print(response.json()['location'])
