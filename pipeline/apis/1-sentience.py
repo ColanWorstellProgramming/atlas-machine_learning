a874287b20817e303c3b2a73722355018a6eb4ce
Colan Worstell <cworstellprogramming@gmail.com>
2024-02-19 16:15:37 -0600

    pycode

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -17,15 +17,15 @@ def sentientPlanets():
         data = response.json()
 
         for species in data['results']:
-            if ('classification' in species and \
+            if ('classification' in species and
                 species['classification'].lower() == 'sentient') or \
-               ('designation' in species and \
+               ('designation' in species and
                 species['designation'].lower() == 'sentient'):
-                if 'homeworld' in species and species['homeworld']:
-                    homeworld_url = species['homeworld']
-                    homeworld_response = requests.get(homeworld_url)
-                    homeworld_data = homeworld_response.json()
-                    planets.append(homeworld_data['name'])
+                    if 'homeworld' in species and species['homeworld']:
+                        homeworld_url = species['homeworld']
+                        homeworld_response = requests.get(homeworld_url)
+                        homeworld_data = homeworld_response.json()
+                        planets.append(homeworld_data['name'])
 
         url = data['next']
 
