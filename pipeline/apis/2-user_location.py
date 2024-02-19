6d4f7e5208c14f6ba94403e5e77c0da1d37cd1e1
Colan Worstell <cworstellprogramming@gmail.com>
2024-02-19 16:20:08 -0600

    2

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -2,22 +2,21 @@
 """
 Imports
 """
+import sys
+import requests
+import time
 
 
 if __name__ == '__main__':
     """
     User Locations
     """
-    import sys
-    import requests
-    import time
-
     response = requests.get(sys.argv[1])
 
     if response.status_code == 200:
         print(response.json()['location'])
     elif response.status_code == 404:
         print("Not found")
-    elif response.status_code == 403:
+    if response.status_code == 403:
         time = (int(response.headers['X-RateLimit-Reset']) - int(time.time()))
         print("Reset in {} min".format(time//60))
