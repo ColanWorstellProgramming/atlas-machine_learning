fbbbd1de3de0810b9ed29d7fe3006af322bb39dd
Colan Worstell <cworstellprogramming@gmail.com>
2024-02-23 09:51:02 -0600

    0

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -9,18 +9,26 @@ def availableShips(passenger_count):
     """
     Get all avaliable ships that can hold a certian amount of passengers
     """
-    url = 'https://swapi.dev/api/starships/'
     ships = []
+    page = 1
+    url = 'https://swapi.dev/api/starships/?page={}'.format(page)
 
-    while url:
+    response = requests.get(url)
+    data = response.json()
+
+    while data['next']:
+        for ship in data['results']:
+
+            if ship['passengers'] == 'n/a':
+                continue
+            if ship['passengers'] == 'unknown':
+                continue
+            if int(ship['passengers'].replace(',', '')) >= passenger_count:
+                ships.append(ship['name'])
+
+        page += 1
+        url = 'https://swapi.dev/api/starships/?page={}'.format(page)
         response = requests.get(url)
         data = response.json()
-
-        for ship in data['results']:
-            if ship['passengers'].isnumeric():
-                if int(ship['passengers']) >= passenger_count:
-                    ships.append(ship['name'])
-
-        url = data['next']
 
     return ships
