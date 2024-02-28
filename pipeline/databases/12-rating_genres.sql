72204a7b6165b7ebc638a3359f16dff584a5821d
Colan Worstell <cworstellprogramming@gmail.com>
2024-02-28 11:48:31 -0600

    fixes

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -1,8 +1,8 @@
 -- SQL
-SELECT tv_genres.name, SUM(ratings.rating)
-AS rating_sum
+SELECT name, SUM(rate)
+AS rating
 FROM tv_genres
 LEFT JOIN tv_show_genres on tv_genres.id=tv_show_genres.genre_id
 LEFT JOIN tv_show_ratings ON tv_show_genres.show_id=tv_show_ratings.show_id
 GROUP BY name
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
