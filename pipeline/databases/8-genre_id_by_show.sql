8eccf1e427abdb98a62af9dbf64e3662266da38f
Colan Worstell <cworstellprogramming@gmail.com>
2024-02-28 11:37:12 -0600

    test

--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- SQL
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
-INNER JOIN tv_show ON tv_shows.id = tv_show_genres.tv_show_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
