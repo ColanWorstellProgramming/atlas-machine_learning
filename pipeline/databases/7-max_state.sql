2b053e507b05919479199dd88982b4f16a9cebad
Colan Worstell <cworstellprogramming@gmail.com>
2024-02-28 11:33:03 -0600

    fix

--- pipeline/databases/7-max_state.sql
+++ pipeline/databases/7-max_state.sql
@@ -1,2 +1,2 @@
 -- SQL
-SELECT state, AVG(value) AS temp FROM temperatures GROUP BY state ORDER BY state ASC;
+SELECT state, AVG(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC;
